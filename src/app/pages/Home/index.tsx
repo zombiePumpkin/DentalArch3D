0861163a697c648483883a972e8a7f9deb9657de
Jose Pena <ze__augusto@hotmail.com>
2023-02-16 20:52:47 -0300

    Chore: Remove Iluminated spheres

--- src/app/pages/Home/index.tsx
+++ src/app/pages/Home/index.tsx
@@ -1,14 +1,14 @@
 // Components
 import ShadedCanvas from 'app/components/Elements/ShadedCanvas'
-import IlluminatedSpheres from 'app/components/Groups/IlluminatedSpheres'
 import DentalArch from 'app/components/Groups/DentalArch'
 
 // Exported component
-export default function Home():JSX.Element {
+const Home = (): JSX.Element => {
   return (
     <ShadedCanvas>
-      <IlluminatedSpheres />
-      {/* <DentalArch /> */}
+      <DentalArch />
     </ShadedCanvas>
   )
 }
+
+export default Home
