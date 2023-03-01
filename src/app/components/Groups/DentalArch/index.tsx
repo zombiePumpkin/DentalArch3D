091c35a0a1a2c5aa2be2c9bbfc1475de5c0b3659
Jose Pena <ze__augusto@hotmail.com>
2023-02-28 23:54:03 -0300

    Chore: Add model context control and model animations

--- src/app/components/Groups/DentalArch/index.tsx
+++ src/app/components/Groups/DentalArch/index.tsx
@@ -1,5 +1,10 @@
+// Dependencies
+import { createRef, useState, useEffect } from 'react'
+
 // Three JS
+import { Group } from 'three'
 import { PerspectiveCamera } from '@react-three/drei' 
+import gsap from 'gsap'
 
 // Components
 import Teeth from 'assets/3D/Teeth'
@@ -8,8 +13,43 @@ import Teeth from 'assets/3D/Teeth'
 import { angleToRadians } from 'helpers/converter'
 import { teethPosition, teethRotation } from 'helpers/positions'
 
+// Local types
+type MainProps = { side?: number }
+
 // Exported component
-const DentalArch = (): JSX.Element => {
+const DentalArch = ({ side = 1 }: MainProps): JSX.Element => {
+  // Component properties
+  const [currentSide, setCurrentSide] = useState(side)
+  const dentalRef: React.Ref<Group> = createRef()
+
+  // Component actions
+  const rotate = () => {
+    if (dentalRef.current) {
+      const [crRotationX, crRotationY, crRotationZ] = teethRotation[currentSide] 
+      const [rotationX, rotationY, rotationZ] = teethRotation[side]
+
+      gsap.fromTo(
+        dentalRef.current.rotation,
+        { x: crRotationX, y: crRotationY, z: crRotationZ },
+        { x: rotationX, y: rotationY, z: rotationZ}
+      )
+      
+      const [crPositionX, crPositionY, crPositionZ] = teethPosition[currentSide]
+      const [positionX, positionY, positionZ] = teethPosition[side]
+    
+      gsap.fromTo(
+        dentalRef.current.position,
+        { x: crPositionX, y: crPositionY, z: crPositionZ },
+        { x: positionX, y: positionY, z: positionZ}
+      )
+    }
+  }
+
+  useEffect(() => {
+    rotate()
+    setCurrentSide(side)
+  }, [side])
+
   return (
     <group>
       {/* Directional Light */}
@@ -23,11 +63,12 @@ const DentalArch = (): JSX.Element => {
       <PerspectiveCamera makeDefault position={[0, 0, 0]} />
       
       {/* Dental arch */}
-      <Teeth 
+      <Teeth
         castShadow
-        position={teethPosition[1]}
-        rotation={teethRotation[1]}
+        position={teethPosition[side]}
+        rotation={teethRotation[side]}
         scale={1}
+        ref={dentalRef}
       />
     </group>
   )
