5a6a779b3f4dac878c9d3207a790a5151132dfb7
Jose Pena <ze__augusto@hotmail.com>
2023-03-30 23:53:24 -0300

    Fix: Fixing model positions

--- src/helpers/positions.ts
+++ src/helpers/positions.ts
@@ -3,21 +3,21 @@ import { angleToRadians } from './converter'
 
 // Types
 type VectorProps = [x: number, y: number, z: number];
-type TeethPositionsProps = VectorProps[]
-type TeethRotationProps = VectorProps[]
+type PositionsProps = VectorProps[]
+type RotationProps = VectorProps[]
 
 // Teeth position set
-const teethPosition: TeethPositionsProps = [
+const teethPosition: PositionsProps = [
   // Left side
-  [-0.65, -1.1, -3.65],
+  [-0.65, -1.2, -3.65],
   // Front side
-  [0, -1.1, -4],
+  [0, -1.2, -4],
   // Right side
-  [0.65, -1.1, -3.65]
+  [0.65, -1.2, -3.65]
 ]
 
 // Teeth rotation set
-const teethRotation: TeethRotationProps = [
+const teethRotation: RotationProps = [
   // Left side
   [angleToRadians(90), 0, angleToRadians(-45)],
   // Front side
@@ -26,4 +26,25 @@ const teethRotation: TeethRotationProps = [
   [angleToRadians(90), 0, angleToRadians(45)]
 ]
 
-export { teethPosition, teethRotation }
+// Superior jaw position set
+const superiorJawPosition: PositionsProps = [
+  // Closed
+  [0, 0, 0],
+  // Open
+  [0, -0.6, -0.3]
+]
+
+// Superior jaw rotation set
+const superiorJawRotation: RotationProps = [
+  // Closed
+  [0, 0, 0],
+  // Open
+  [angleToRadians(30), angleToRadians(0), angleToRadians(0)]
+]
+
+export {
+  teethPosition,
+  teethRotation,
+  superiorJawPosition,
+  superiorJawRotation
+}
